43f39c063150a947c2b7e47db3a8a2b9baa79941
Mina Kim <rmaomina@gmail.com>
2023-02-02 20:57:58 +0900

    chore: 환경변수 적용

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,5 @@
-const BASE_URL = process.env.REACT_APP_BASE_URL;
-const BASE_URL2 = process.env.REACT_APP_BASE_URL2;
+const BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const BASE_URL2 = process.env.REACT_APP_API_BASE_URL2;
 
 export function fetchCoins() {
   return fetch(`${BASE_URL}/coins`).then(res => res.json());
@@ -14,7 +14,5 @@ export function fetchCoinPrice(coinId: string) {
 }
 
 export function fetchCoinHistory(coinId: string) {
-  // const endDate = Math.floor(Date.now() / 1000);
-  // const startDate = endDate - 60 * 60 * 24 * 7;
   return fetch(`${BASE_URL2}?coinId=${coinId}`).then(res => res.json());
 }
